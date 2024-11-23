4690964c954395a1e539ef3c364126aa5c947fd1
ValentinGuillot <97942568+mrvalbass@users.noreply.github.com>
2024-11-23 10:53:08 +0100

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   BACKGROUND_PRIMARY,
   BACKGROUND_SECONDARY,
@@ -6,25 +7,42 @@ import {
 } from "../app/constants/colors";
 import Contacts from "./Contacts";
 import Socials from "./Socials";
+import { CLICKABLE } from "@/app/constants/style";
 
 export default function Footer() {
   return (
-    <footer
-      className={`flex w-full h-[10svh] md:h-[15svh] max-h-[130px] ${BACKGROUND_PRIMARY} `}
-    >
-      <div className={`h-full w-1/2 ${BACKGROUND_SECONDARY}`}>
-        <div
-          className={`flex ${BACKGROUND_PRIMARY} ${TEXT_PRIMARY} h-full pl-10 md:pl-24 rounded-br-[5svh]`}
-        >
-          <Contacts />
+    <footer>
+      <div
+        className={`flex h-[10svh] max-h-[130px] w-full md:h-[15svh] ${BACKGROUND_PRIMARY} `}
+      >
+        <div className={`h-full w-1/2 ${BACKGROUND_SECONDARY}`}>
+          <div
+            className={`flex ${BACKGROUND_PRIMARY} ${TEXT_PRIMARY} h-full rounded-br-[5svh] pl-10 md:pl-24`}
+          >
+            <Contacts />
+          </div>
+        </div>
+        <div className={`h-full w-1/2 ${BACKGROUND_PRIMARY}`}>
+          <div
+            className={`flex ${BACKGROUND_SECONDARY} ${TEXT_SECONDARY} h-full items-center justify-end rounded-tl-[5svh] pr-10 md:pr-24`}
+          >
+            <Socials />
+          </div>
         </div>
       </div>
-      <div className={`h-full w-1/2 ${BACKGROUND_PRIMARY}`}>
-        <div
-          className={`flex ${BACKGROUND_SECONDARY} ${TEXT_SECONDARY} h-full pr-10 md:pr-24 rounded-tl-[5svh] items-center justify-end`}
+      <div
+        className={`flex justify-around py-1 ${BACKGROUND_SECONDARY} ${TEXT_SECONDARY}`}
+      >
+        <Link href="#" className={`font-Hanken text-xs ${CLICKABLE}`}>
+          Mentions Légales
+        </Link>
+        <Link
+          href="https://valentinguillot.fr"
+          target="_blank"
+          className={`font-Hanken text-xs ${CLICKABLE}`}
         >
-          <Socials />
-        </div>
+          ©Valentin Guillot - 2024
+        </Link>
       </div>
     </footer>
   );
